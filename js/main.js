a3b4ba2816956a217e1d59b8f30adb5d5a785406
oleg_nexxxt <nexxxt.07@gmail.com>
2021-06-21 18:51:43 +0300

    21.06

--- js/main.js
+++ js/main.js
@@ -3,5 +3,16 @@ if (window.innerWidth > 768) {
     let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
         return new bootstrap.Popover(popoverTriggerEl)
     })
+} else {
+
+    let mobileSliderInfo = new Swiper("#mobileSliderInfo", {
+        slidesPerView: "auto",
+        spaceBetween: 10,
+    });
+    let mobSliderTab = new Swiper("#mobSliderTab", {
+        slidesPerView: "auto",
+        spaceBetween: 20,
+    });
+
 }
 
